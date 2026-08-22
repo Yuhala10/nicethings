a038368534e459536f49c1b93af3403c17d8264b
Yuhala10 <yuhala24@gmail.com>
2026-08-22 20:31:17 +0100

    Fix production build and global language support

--- pages/_app.js
+++ pages/_app.js
@@ -1,43 +1,32 @@
-import {
-    useEffect,
-} from "react";
-
+import { useEffect } from "react";
 import "../styles/globals.css";
-
-import {
-    initializeVisitor,
-} from "../lib/visitor";
+import { LanguageProvider } from "../lib/i18n";
 
 export default function App({
     Component,
     pageProps,
 }) {
     useEffect(() => {
-        initializeVisitor(
-            "en"
-        );
-
         if (
             "serviceWorker" in
             navigator
         ) {
             navigator.serviceWorker
-                .register(
-                    "/sw.js"
-                )
-                .catch(
-                    (error) =>
-                        console.error(
-                            "Service worker:",
-                            error
-                        )
-                );
+                .register("/sw.js")
+                .catch((error) => {
+                    console.error(
+                        "Service worker:",
+                        error
+                    );
+                });
         }
     }, []);
 
     return (
-        <Component
-            {...pageProps}
-        />
+        <LanguageProvider>
+            <Component
+                {...pageProps}
+            />
+        </LanguageProvider>
     );
 }
